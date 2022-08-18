30b7d5490ff83c92033fccab96d5764a3da3eeb5
Devopsik404 <kirill.laschenko111@gmail.com>
2022-08-18 12:45:06 +0300

    edit navigation

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,5 @@
 export const nav = `<nav class="navbar navbar-expand-lg navbar-light fixed-top">
-   <a class="navbar-brand" href="#"> <img src="/images/cropped-mein_freund_mykola.png" alt="Mykola" width="30px"></a>
+   <a class="navbar-brand" href="/pages#"> <img src="/images/cropped-mein_freund_mykola.png" alt="Mykola" width="30px"></a>
    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
       aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -11,130 +11,130 @@ export const nav = `<nav class="navbar navbar-expand-lg navbar-light fixed-top">
          <li class="nav-item active">
             <div class="main-page-link">
                <img class="pig_image" src="/images/cropped-mein_freund_mykola.png" alt="" height="55">
-               <a class="nav-link " href="/">  Головна - Principal  </a>
+               <a class="nav-link " href="/index.html">  Головна - Principal  </a>
             </div>
          </li>
          <li class="nav-item">
-            <a class="nav-link nav-link-collapse" href="/moldova" id="hasSubItems" data-toggle="collapse"
+            <a class="nav-link nav-link-collapse" href="/pages/moldova.html" id="hasSubItems" data-toggle="collapse"
                data-target="#collapseSubItems2" aria-controls="collapseSubItems2" aria-expanded="false">Мапа - Hartă</a>
             <ul class="nav-second-level collapse" id="collapseSubItems2" data-parent="#navAccordion">
                <li class="nav-item">
-                  <a class="nav-link" href="/moldova">
+                  <a class="nav-link" href="/pages/moldova.html">
                      <span class="nav-link-text">Про Молдову - Despre Moldova</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/nord">
+                  <a class="nav-link" href="/pages/nord.html">
                      <span class="nav-link-text">Північ – Nord</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/vest">
+                  <a class="nav-link" href="/pages/vest.html">
                      <span class="nav-link-text">Захід – Vest</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/est">
+                  <a class="nav-link" href="/pages/est.html">
                      <span class="nav-link-text">Схід – Est</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/sud">
+                  <a class="nav-link" href="/pages/sud.html">
                      <span class="nav-link-text">Південь – Sud</span>
                   </a>
                </li>
             </ul>
          </li>
          <li class="nav-item">
-            <a class="nav-link nav-link-collapse" href="#" id="hasSubItems" data-toggle="collapse"
+            <a class="nav-link nav-link-collapse" href="/pages#" id="hasSubItems" data-toggle="collapse"
                data-target="#collapseSubItems4" aria-controls="collapseSubItems4" aria-expanded="false"> Словник -
                Vocabular </a>
             <ul class="nav-second-level collapse" id="collapseSubItems4" data-parent="#navAccordion">
                <li class="nav-item">
-                  <a class="nav-link" href="/slova">
+                  <a class="nav-link" href="/pages/slova.html">
                      <span class="nav-link-text">Важливі слова –
                         Cuvinte importante</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/familie">
+                  <a class="nav-link" href="/pages/familie.html">
                      <span class="nav-link-text">Сім’я – Familie</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/viata-de-zi-cu-zi">
+                  <a class="nav-link" href="/pages/viata-de-zi-cu-zi.html">
                      <span class="nav-link-text">Повсякденне життя – Viata de zi cu zi</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/vidpochinok">
+                  <a class="nav-link" href="/pages/vidpochinok.html">
                      <span class="nav-link-text">Вільний час - timp liber</span>
                   </a>
                </li>
                
                <li class="nav-item">
-                  <a class="nav-link" href="/cumparaturi">
+                  <a class="nav-link" href="/pages/cumparaturi.html">
                      <span class="nav-link-text">Покупки – Cumpărături</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/scoala">
+                  <a class="nav-link" href="/pages/scoala.html">
                      <span class="nav-link-text">Школа – Şcoală</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/alimente">
+                  <a class="nav-link" href="/pages/alimente.html">
                      <span class="nav-link-text">Їжа – Alimente</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/sanatate">
+                  <a class="nav-link" href="/pages/sanatate.html">
                      <span class="nav-link-text">Здоров’я – Sănătate</span>
                   </a>
                </li>
             </ul>
          </li>
          <li class="nav-item">
-            <a class="nav-link nav-link-collapse" href="/ajutor" id="hasSubItems" data-toggle="collapse"
+            <a class="nav-link nav-link-collapse" href="/pages/ajutor.html" id="hasSubItems" data-toggle="collapse"
                data-target="#collapseSubItems3" aria-controls="collapseSubItems3" aria-expanded="false"> Допомога -
                Ajutor</a>
             <ul class="nav-second-level collapse" id="collapseSubItems3" data-parent="#navAccordion">
                <li class="nav-item">
-                  <a class="nav-link" href="/ajutor">
+                  <a class="nav-link" href="/pages/ajutor.html">
                      <span class="nav-link-text">Хто може мені допомогти? – Cine ma poate
                         ajuta?</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/politia">
+                  <a class="nav-link" href="/pages/politia.html">
                      <span class="nav-link-text">Поліція – Poliția</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/pompierii">
+                  <a class="nav-link" href="/pages/pompierii.html">
                      <span class="nav-link-text">Пожежники – Pompierii</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/doctor">
+                  <a class="nav-link" href="/pages/doctor.html">
                      <span class="nav-link-text">Лікарня та Лікар – Spital și Doctor</span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/primaria">
+                  <a class="nav-link" href="/pages/primaria.html">
                      <span class="nav-link-text">Мерія та муніципалітет – Primărie și
                         primărie </span>
                   </a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="/voluntari">
+                  <a class="nav-link" href="/pages/voluntari.html">
                      <span class="nav-link-text">Волонтери – Voluntari</span>
                   </a>
                </li>
             </ul>
          </li>
          <li class="nav-item active">
-            <a class="nav-link" href="/asistenti">Помічники Миколи – Asistenții lui Mykola</a>
+            <a class="nav-link" href="/pages/asistenti.html">Помічники Миколи – Asistenții lui Mykola</a>
          </li>
       </ul>
    </div>
--- mykola/goto.js
+++
@@ -1,5 +0,0 @@
-function gotoMainPage() {
-    location.replace("http://mij-drug-mykola-u-moldovi.42web.io");
-}
-
-document.body.addEventListener("onload",gotoMainPage());
