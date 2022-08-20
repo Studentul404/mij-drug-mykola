72f47d73c9a843e251d13e06394a72bd5998095b
Devopsik404 <kirill.laschenko111@gmail.com>
2022-08-20 11:19:16 +0300

    Remove routing

--- js/content.js
+++ js/content.js
@@ -3,9 +3,6 @@ import {
 } from "./navigation.js";
 
 $(document).ready(function () {
-   if (location.href.endsWith("main.html")) {
-      location.href = "http://mij-drug-mykola-u-moldovi.42web.io/";
-   }
    document.querySelector("#nav").innerHTML = nav;
    $('.nav-link-collapse').on('click', function () {
       $('.nav-link-collapse').not(this).removeClass('nav-link-show');
