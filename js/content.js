900ddfee7b2352df3f41f854d2df6fe81bfc7764
Devopsik404 <kirill.laschenko111@gmail.com>
2022-08-20 11:10:00 +0300

    Lint and add routing

--- js/content.js
+++ js/content.js
@@ -1,9 +1,15 @@
-import {nav} from "./navigation.js";
+import {
+   nav
+} from "./navigation.js";
 
 $(document).ready(function () {
+   if (location.href.endsWith("main.html")) {
+      location.href = "http://mij-drug-mykola-u-moldovi.42web.io/";
+   }
    document.querySelector("#nav").innerHTML = nav;
    $('.nav-link-collapse').on('click', function () {
       $('.nav-link-collapse').not(this).removeClass('nav-link-show');
       $(this).toggleClass('nav-link-show');
    });
+
 });
